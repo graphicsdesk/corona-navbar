54f0eca4634e37be2e95ca765924f716f2de2b6e
Jason Kao <jason.kao@columbia.edu>
2020-03-14 22:17:35 -0400

    dynamic nav links

--- src/script.js
+++ src/script.js
@@ -7,3 +7,39 @@ export function hamburgerTrigger() {
 }
 
 // const navLinksWidth = navbar.clientWidth - document.getElementById('cds-logo-desktop-container').clientWidth;
+
+// Creating nav links
+
+const navItems = [
+  {
+    name: 'Live updates',
+    link: '/',
+  },
+  {
+    name: 'Housing and accommodations',
+    link: '',
+  },
+  {
+    name: 'Academic resources',
+    link: '',
+  },
+  {
+    name: 'Staying on campus',
+    link: '',
+  },
+];
+
+const navLinks = document.getElementById('nav-links');
+const { pathname } = window.location;
+
+navItems.forEach(({ name, link}) => {
+  const div = document.createElement('div');
+  div.classList.add('nav-link');
+  if (link.includes(pathname))
+    div.classList.add('nav-link-highlighted');
+  const a = document.createElement('a');
+  a.setAttribute('href', link);
+  a.innerText = name;
+  div.append(a);
+  navLinks.append(div);
+});
