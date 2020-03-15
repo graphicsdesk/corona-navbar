2a1976327ac0d1647a653f9b05e64801e19b91b8
Jason Kao <jason.kao@columbia.edu>
2020-03-15 14:11:25 -0400

    remove navbar and styles if on a contributor page

--- src/script.js
+++ src/script.js
@@ -5,40 +5,53 @@ export function hamburgerTrigger() {
   navbar.classList.toggle('show-nav-links');
 }
 
-// Creating nav links
+// Hide everything if on a contributor page
 
-const navItems = [
-  {
-    name: 'News updates',
-    link: 'https://www.columbiaspectator.com/news/2020/03/15/coronavirus-updates-columbia-and-barnard-move-to-online-classes-for-remainder-of-spring-semester',
-  },
-  {
-    name: 'Housing and accommodations',
-    link: 'https://www.columbiaspectator.com/news/2020/03/15/how-to-navigate-housing-and-university-accommodations/',
-  },
-  {
-    name: 'Academic resources',
-    link: 'https://www.columbiaspectator.com/news/2020/03/15/navigating-a-virtual-academic-landscape/',
-  },
-  {
-    name: 'Staying on campus',
-    link: 'https://www.columbiaspectator.com/news/2020/03/15/what-you-need-to-know-if-you-remain-on-campus/',
-  },
-];
-
-const navLinks = document.getElementById('nav-links');
 const { pathname } = window.location;
 
-navItems.forEach(({ name, link}) => {
-  const div = document.createElement('div');
-  div.classList.add('nav-link');
-  if (link.includes(pathname))
-    div.classList.add('nav-link-highlighted');
+if (pathname.indexOf('/contributors') === 0) {
+  const stylesLink = document.querySelector('.story-summary .twolines link');
+  stylesLink.parentNode.removeChild(stylesLink);
+  navbar.style.display = 'none';
+} else {
+  createNavLinks();
+}
 
-  const a = document.createElement('a');
-  a.setAttribute('href', link);
-  a.innerText = name;
+// Creating nav links
 
-  div.append(a);
-  navLinks.append(div);
-});
+function createNavLinks() {
+  const navItems = [
+    {
+      name: 'News updates',
+      link: 'https://www.columbiaspectator.com/news/2020/03/15/coronavirus-updates-columbia-and-barnard-move-to-online-classes-for-remainder-of-spring-semester',
+    },
+    {
+      name: 'Housing and accommodations',
+      link: 'https://www.columbiaspectator.com/news/2020/03/15/how-to-navigate-housing-and-university-accommodations/',
+    },
+    {
+      name: 'Academic resources',
+      link: 'https://www.columbiaspectator.com/news/2020/03/15/navigating-a-virtual-academic-landscape/',
+    },
+    {
+      name: 'Staying on campus',
+      link: 'https://www.columbiaspectator.com/news/2020/03/15/what-you-need-to-know-if-you-remain-on-campus/',
+    },
+  ];
+
+  const navLinks = document.getElementById('nav-links');
+
+  navItems.forEach(({ name, link}) => {
+    const div = document.createElement('div');
+    div.classList.add('nav-link');
+    if (link.includes(pathname))
+      div.classList.add('nav-link-highlighted');
+
+    const a = document.createElement('a');
+    a.setAttribute('href', link);
+    a.innerText = name;
+
+    div.append(a);
+    navLinks.append(div);
+  });
+}
